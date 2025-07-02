76009e076d87130813fb15b860902dfc171fd540
J-Norbi <janko.norbert.zoltan@gmail.com>
2025-07-02 19:34:42 +0200

    Update server.js
    role:userLogin.role kiegészítés 146.sor

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.post('/login', async (req,res) => {
             {id: userLogin.id, email: userLogin.email, role: userLogin.role}, SECRET, { expiresIn: '1h' } 
         )
 
-        return res.status(200).json({token, message: 'Sikeres bejelentkezés!'})
+        return res.status(200).json({token, role:userLogin.role, message: 'Sikeres bejelentkezés!'})
 
     }
     catch(error){
