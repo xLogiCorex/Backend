4e37f1149650c36cc2a98fdf625e83fa4217ba7a
Petrusheen <nyaripetra.ronett1@gmail.com>
2025-09-08 19:56:24 +0200

    Updated Backend

--- orders.js
+++ orders.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const { orderTable, orderItemTable, productTable, invoiceTable } = require('./dbHandler');
+const express = require('express')
+const router = express.Router()
+const { orderTable, orderItemTable, productTable, stockMovementTable } = require('./dbHandler')
 
 const authenticateJWT = require('./authenticateJWT');
 const authorizeRole = require('./authorizeRole');
